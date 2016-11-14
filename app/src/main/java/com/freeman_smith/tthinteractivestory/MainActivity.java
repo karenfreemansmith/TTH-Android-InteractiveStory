df38339626a3cf87e9dcb67ab59c88b0582ad555
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-11-13 18:52:52 -0800

    add main title page

---
+++ app/src/main/java/com/freeman_smith/tthinteractivestory/MainActivity.java
@@ -0,0 +1,13 @@
+package com.freeman_smith.tthinteractivestory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
